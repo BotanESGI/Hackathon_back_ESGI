4b5d2ce95c943a07ece5a0fb701ea59956142c5a
Botan YILMAZ <byilmaz1@myges.fr>
2025-04-24 14:45:04 +0200

    generation de jwt

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-# Wait for database to be ready
+# Générer les clés si elles n'existent pas
+# Définir la paraphrase (remplace-la par ta propre paraphrase si nécessaire)
+JWT_PASSPHRASE="16a1417682d1ad62b39f5e48c81dc665a94c3f43031fd8fd8c6d45f5a6078332"
 
+# Vérifier si les fichiers de clés existent, sinon les générer
+if [ ! -f config/jwt/private.pem ]; then
+  mkdir -p config/jwt
+  openssl genrsa -out config/jwt/private.pem -aes256 -passout pass:$JWT_PASSPHRASE 4096
+  openssl rsa -pubout -in config/jwt/private.pem -passin pass:$JWT_PASSPHRASE -out config/jwt/public.pem
+fi
+
+# Continue with Symfony build
+composer install --no-interaction --optimize-autoloader
+php bin/console cache:clear
+
+# Wait for database to be ready
 echo "Database is ready"
 
 # Create database and run migrations
@@ -20,4 +34,4 @@ php -S 0.0.0.0:8000 -t public &
 SERVER_PID=$!
 
 # Keep the container running
-wait $SERVER_PID 
+wait $SERVER_PID
